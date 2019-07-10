deb144bffdf5ed9587ad2f8cea4deac5006032da
Eric Johnson <ericdj@amazon.com>
2019-07-10 12:18:10 -0700

    Initial commit

---
+++ src/layouts/Default.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated class="glossy">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar font-size="26px" size="40px" color="red" text-color="white">#</q-avatar>ServerlessForEveryone
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'LayoutDefault',
+
+  data () {
+    return {
+      leftDrawerOpen: this.$q.platform.is.desktop
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './quasar'
+import Amplify, * as AmplifyModules from 'aws-amplify'
+import { AmplifyPlugin } from 'aws-amplify-vue'
+import awsconfig from './aws-exports'
+Amplify.configure(awsconfig)
+
+Vue.use(AmplifyPlugin, AmplifyModules)
 
 Vue.config.productionTip = false
 
